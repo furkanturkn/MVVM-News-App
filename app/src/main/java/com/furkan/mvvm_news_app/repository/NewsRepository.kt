8d69b736c975325d870a951a046a8b18c8d700d7
furki <furkan.turkan1@bahcesehir.edu.tr>
2023-08-10 10:06:47 +0300

    [feat] add no result to error handling logic

--- app/src/main/java/com/furkan/mvvm_news_app/repository/NewsRepository.kt
+++ app/src/main/java/com/furkan/mvvm_news_app/repository/NewsRepository.kt
@@ -1,9 +1,11 @@
 package com.furkan.mvvm_news_app.repository
 
+import com.furkan.mvvm_news_app.R
 import com.furkan.mvvm_news_app.data.remote.NewsApi
 import com.furkan.mvvm_news_app.data.remote.responses.NewsResponse
 import com.furkan.mvvm_news_app.util.ApiResponseUtils.handleException
 import com.furkan.mvvm_news_app.util.Resource
+import com.furkan.mvvm_news_app.util.UiText
 import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Inject
 
@@ -25,7 +27,15 @@ class NewsRepository @Inject constructor(
                 fromDate = fromDate,
                 toDate = toDate
             )
-            Resource.Success(response)
+            if(response.totalResults == 0) {
+                Resource.Error(
+                    UiText.StringResource(R.string.no_result)
+                )
+            }
+            else{
+                Resource.Success(response)
+            }
+
         } catch (e: Throwable) {
             handleException(e)
         }
